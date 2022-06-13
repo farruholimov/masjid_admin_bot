c9aa3dc332efb7cfc75800ad8ffaf52a6485b5e2
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2022-06-13 10:28:35 +0500

    fix

--- bot.js
+++ bot.js
@@ -69,148 +69,148 @@ bot.on("callback_query:data", async ctx => {
     }
 })
 
-bot.use(router)
+// bot.use(router)
 bot.start()
 
-bot.command("start", async (ctx, next) => {
-    const chat_id = ctx.msg.chat.id
+// bot.command("start", async (ctx, next) => {
+//     const chat_id = ctx.msg.chat.id
     
-    let user = await getUser(ctx)
+//     let user = await getUser(ctx)
     
-    if(user && (user.role != 2)){
-        await sendAlert(ctx, "Siz bu botdan foydalana olmaysiz")
-        return
-    }
+//     if(user && (user.role != 2)){
+//         await sendAlert(ctx, "Siz bu botdan foydalana olmaysiz")
+//         return
+//     }
     
-    if(user && (user["mosque_admin.verified"] == false)){
-        await sendAlert(ctx, "Siz hali tasdiqlanmagansiz")
-        return
-    }
+//     if(user && (user["mosque_admin.verified"] == false)){
+//         await sendAlert(ctx, "Siz hali tasdiqlanmagansiz")
+//         return
+//     }
     
-    if (!user) {
-        console.log(chat_id);
-        let body = {
-            telegram_id: chat_id,
-            role: 2,
-            step: "name"
-        }
-        user = await fetchUrl(`/users`, "POST", body)
-        user = user?.data.user
+//     if (!user) {
+//         console.log(chat_id);
+//         let body = {
+//             telegram_id: chat_id,
+//             role: 2,
+//             step: "name"
+//         }
+//         user = await fetchUrl(`/users`, "POST", body)
+//         user = user?.data.user
 
-        ctx.session.user.tgid = chat_id
-        ctx.session.user.id = user.id
-        ctx.session.step = "name"
-        await askName(ctx)
-        return
-    }
+//         ctx.session.user.tgid = chat_id
+//         ctx.session.user.id = user.id
+//         ctx.session.step = "name"
+//         await askName(ctx)
+//         return
+//     }
 
 
-    ctx.session.user = {
-        tgid: chat_id,
-        id: user.id,
-        name: user.full_name,
-        phone: user.phone_number,
-    }
+//     ctx.session.user = {
+//         tgid: chat_id,
+//         id: user.id,
+//         name: user.full_name,
+//         phone: user.phone_number,
+//     }
 
-    ctx.session.step = user.step
-})
+//     ctx.session.step = user.step
+// })
 
-bot.on("message", async (ctx, next) => {
-    const chat_id = ctx.msg.chat.id
+// bot.on("message", async (ctx, next) => {
+//     const chat_id = ctx.msg.chat.id
 
-    let user = await getUser(ctx)
+//     let user = await getUser(ctx)
 
-    if(user && (user.role != 2)){
-        await sendAlert(ctx, "Siz bu botdan foydalana olmaysiz")
-        return
-    }
+//     if(user && (user.role != 2)){
+//         await sendAlert(ctx, "Siz bu botdan foydalana olmaysiz")
+//         return
+//     }
     
-    if(ctx.session.step == "verification"){
-        if(user && (!user["mosque_admin.verified"])){
-            await sendAlert(ctx, "Siz hali tasdiqlanmagansiz")
-            return
-        }
-    }
+//     if(ctx.session.step == "verification"){
+//         if(user && (!user["mosque_admin.verified"])){
+//             await sendAlert(ctx, "Siz hali tasdiqlanmagansiz")
+//             return
+//         }
+//     }
 
-    ctx.session.user = {
-        tgid: chat_id,
-        id: user.id,
-        name: user.full_name,
-        phone: user.phone_number,
-    }
+//     ctx.session.user = {
+//         tgid: chat_id,
+//         id: user.id,
+//         name: user.full_name,
+//         phone: user.phone_number,
+//     }
 
-    ctx.session.step = user.step
-    next()
-})
+//     ctx.session.step = user.step
+//     next()
+// })
 
-bot.command("menu", async (ctx) => {
-    let user = await getUser(ctx)
+// bot.command("menu", async (ctx) => {
+//     let user = await getUser(ctx)
 
-    if(!user){
-        await ctx.reply("Siz ro'yxatdan o'tmagansiz! Ro'yhatdan o'tish uchun /start buyrug'ini jo'nating")
-        return
-    }
-    await sendMenu(ctx)
-})
+//     if(!user){
+//         await ctx.reply("Siz ro'yxatdan o'tmagansiz! Ro'yhatdan o'tish uchun /start buyrug'ini jo'nating")
+//         return
+//     }
+//     await sendMenu(ctx)
+// })
 
-const router = new Router(ctx => ctx.session.step)
+// const router = new Router(ctx => ctx.session.step)
 
-router.route("name", async ctx => {
-    await setName(ctx)
-    ctx.session.step = "phone"
-    await askPhone(ctx)
-    await updateUserStep(ctx, ctx.session.step)
-})
+// router.route("name", async ctx => {
+//     await setName(ctx)
+//     ctx.session.step = "phone"
+//     await askPhone(ctx)
+//     await updateUserStep(ctx, ctx.session.step)
+// })
 
-router.route("phone", async (ctx) => {
-    let p = await setPhone(ctx)
-    if (!p) return
-    ctx.session.step = "select_mosque"
-    await updateUserStep(ctx, ctx.session.step)
-    await selectMosque(ctx)
-})
+// router.route("phone", async (ctx) => {
+//     let p = await setPhone(ctx)
+//     if (!p) return
+//     ctx.session.step = "select_mosque"
+//     await updateUserStep(ctx, ctx.session.step)
+//     await selectMosque(ctx)
+// })
 
-router.route("select_mosque", async (ctx) => {
-    await selectMosque(ctx, "select_category")
-})
-router.route("menu", async (ctx) => {
-    await sendMenu(ctx)
-})
+// router.route("select_mosque", async (ctx) => {
+//     await selectMosque(ctx, "select_category")
+// })
+// router.route("menu", async (ctx) => {
+//     await sendMenu(ctx)
+// })
 
-bot.on("callback_query:data", async ctx => {
-    const {
-        url: command,
-        query
-    } = require('query-string').parseUrl(ctx.callbackQuery.data)
+// bot.on("callback_query:data", async ctx => {
+//     const {
+//         url: command,
+//         query
+//     } = require('query-string').parseUrl(ctx.callbackQuery.data)
 
-    switch (command) {
-        case "all_mosques":
-            await openSettingsMenu(ctx)
-            break;
-        case "settings":
-            await openSettingsMenu(ctx)
-            break;
-        case "my_categories":
-            await selectMosque(ctx, "remove_category")
-            break;
-        case "back":
-            await backToMenu(ctx)
-            break;
+//     switch (command) {
+//         case "all_mosques":
+//             await openSettingsMenu(ctx)
+//             break;
+//         case "settings":
+//             await openSettingsMenu(ctx)
+//             break;
+//         case "my_categories":
+//             await selectMosque(ctx, "remove_category")
+//             break;
+//         case "back":
+//             await backToMenu(ctx)
+//             break;
     
-        default:
-            break;
-    }
-})
+//         default:
+//             break;
+//     }
+// })
 
-bot.catch((err) => {
-    const ctx = err.ctx;
-    console.error(`Error while handling update ${ctx.update.update_id}:`);
-    const e = err.error;
-    if (e instanceof GrammyError) {
-        console.error("Error in request:", e.description);
-    } else if (e instanceof HttpError) {
-        console.error("Could not contact Telegram:", e);
-    } else {
-        console.error("Unknown error:", e);
-    }
-});
+// bot.catch((err) => {
+//     const ctx = err.ctx;
+//     console.error(`Error while handling update ${ctx.update.update_id}:`);
+//     const e = err.error;
+//     if (e instanceof GrammyError) {
+//         console.error("Error in request:", e.description);
+//     } else if (e instanceof HttpError) {
+//         console.error("Could not contact Telegram:", e);
+//     } else {
+//         console.error("Unknown error:", e);
+//     }
+// });
