0324e6ea9ab31919b422fb147b2139803abc06ad
farruholimov <farruhbekolimov3904@gmail.com>
2022-07-21 11:56:44 +0500

    fix

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -188,7 +188,7 @@ class Controllers {
     // MENU
 
     static async sendMenu(ctx, additional) {
-        const user = await this.getUser(ctx) 
+        const user = await Controllers.getUser(ctx) 
         const notification_count = await fetchUrl(`/notifications?user_id=${user.id}&count=true`)
         await ctx.reply((additional ? additional + "\n" : '') + messages.menuMsg, {
             parse_mode: "HTML",
